a8b70e80d297982eb6dc5a28d67e4b369bf61d26
PedroHenriqueAraujo18 <112351827+PedroHenriqueAraujo18@users.noreply.github.com>
2024-09-16 15:43:33 -0300

    Adicionando Estudos da Interface List

---
+++ Estudos de List/src/ListaTarefa.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> lista;
+
+    public ListaTarefa() {
+        lista = new ArrayList<Tarefa>();
+    }
+    public void adicionaTarefa(String descricao) {
+        lista.add(new Tarefa(descricao));
+    }
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefaRemover = new ArrayList<>();
+        for (Tarefa tarefa : lista) {
+        if(tarefa.getDescricao().equalsIgnoreCase(descricao)) {
+            tarefaRemover.add(tarefa);
+            }
+        }
+        lista.removeAll(tarefaRemover);
+    }
+    public int obterNumeroTotalTarefas() {
+        return lista.size();
+    }
+    public void obterDescricaoTarefas() {
+       System.out.print(lista);
+    }
+}
